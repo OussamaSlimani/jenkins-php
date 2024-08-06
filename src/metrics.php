b085e57fd2344c26386a2f155ea570cfa4e1da98
OussamaSlimani <slimani.oussama@enis.tn>
2024-08-06 07:58:37 +0100

    yalla

--- src/metrics.php
+++ src/metrics.php
@@ -1,22 +1,10 @@
 <?php
-// metrics.php
-header('Content-Type: text/plain; version=0.0.4; charset=utf-8');
+header('Content-Type: application/json');
 
-// Define a function to get metrics
-function getMetrics() {
-    // Example metrics
-    $metrics = [
-        "# HELP php_requests_total The total number of HTTP requests.",
-        "# TYPE php_requests_total counter",
-        "php_requests_total{method=\"get\"} 1027",
-        "php_requests_total{method=\"post\"} 3",
-        "",
-        "# HELP php_errors_total The total number of errors.",
-        "# TYPE php_errors_total counter",
-        "php_errors_total 42",
-    ];
+$metrics = [
+    'uptime' => shell_exec('uptime'),
+    'memory_usage' => memory_get_usage(),
+    // Add more metrics as needed
+];
 
-    return implode("\n", $metrics);
-}
-
-echo getMetrics();
+echo json_encode($metrics);
